f5092ea4ee863032df2e63fb206cf8e0dceed7ed
nithi29 <nithi.subri@gmail.com>
2018-05-14 17:37:08 +0530

    from eclipse

---
+++ workspace/Helloworld/src/Simple.java
@@ -0,0 +1,16 @@
+
+public class Simple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!!!");
+int x=2;
+System.out.println(x);
+
+		
+	}
+
+}
